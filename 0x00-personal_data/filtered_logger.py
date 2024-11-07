d24aac18f18e98b8fcef695c193edf7fcb7ea081
flowsar <brahim.sar151@gmail.com>
2024-11-08 00:24:04 +0100

    doc

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -23,10 +23,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """initialize attribute"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ format method"""
         return filter_datum(self.fields,
                             RedactingFormatter.REDACTION,
                             message, RedactingFormatter.SEPARATOR)
