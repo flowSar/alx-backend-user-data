b014092378ad13a71fbc9ffbaac82e9d79c94f52
flowsar <brahim.sar151@gmail.com>
2024-11-12 23:34:37 +0100

    Define which routes don't need authentication

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -10,6 +10,16 @@ class Auth:
     """class auth"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Determines if authentication is required for a given path """
+        if path not in excluded_paths or path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if path in excluded_paths:
+            return False
+        if '/api/v1/status/' in excluded_paths:
+            return False
+        if '/api/v1/status' in excluded_paths:
+            return False
         return False
 
     def authorization_header(self, request=None) -> str:
